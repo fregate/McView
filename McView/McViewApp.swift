601d95b216593c9732bdd0ec463c605f840ee672
Anton Filippov <fregate@ngs.ru>
2022-06-18 12:13:49 +0700

    Initial Commit

---
+++ McView/McViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  McViewApp.swift
+//  McView
+//
+//  Created by Anton Filippov on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct McViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
